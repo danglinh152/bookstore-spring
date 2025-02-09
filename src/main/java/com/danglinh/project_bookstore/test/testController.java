7af33fd7a6b55eb673ac78e1b53343c2241e4646
danglinh152 <danglinh.k4@gmail.com>
2025-02-09 20:19:07 +0700

    complete File Service and API Upload File

--- src/main/java/com/danglinh/project_bookstore/test/testController.java
+++ src/main/java/com/danglinh/project_bookstore/test/testController.java
@@ -4,11 +4,18 @@ package com.danglinh.project_bookstore.test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+
 @RestController
 public class testController {
 
     @GetMapping("/")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/get-avatar")
+    public File getAvatar() {
+        return new File("D:/fullstack/upload/folder/test.png");
     }
 }
